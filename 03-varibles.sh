22001b7a7a0678244efab067bae8e83239e9ed8f
Lakshmanpagala <laxman.gdn@gmail.com>
2025-01-05 10:07:45 +0530

    Shell-script-practice

---
+++ 03-varibles.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Laxman
+PERSON2=Ram
+
+echo "$PERSON1:Hi!! $PERSON2"
+echo "$PERSON2:Hello!! $PERSON1"
+echo "$PERSON1:How are you $PERSON2"
+echo "$PERSON2:iam fine, what about you $PERSON1"
