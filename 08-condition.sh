685e34bf749dfcda5ee178aab976e3c11fd69761
Lakshmanpagala <laxman.gdn@gmail.com>
2025-01-05 10:57:03 +0530

    Shell-script-practice

--- 08-condition.sh
+++ 08-condition.sh
@@ -6,8 +6,6 @@ NUMBER=$1
 if [ $NUMBER -gt 100 ]
 then
     echo "Given number greater than 100"
-elif
-    echo "Given number less than 100"
 else 
     echo "Given number greater than or equal to 100"
 fi
