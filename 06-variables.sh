8ae9b236d7d1c9857b0f1bc06a0148e154a7713e
Lakshmanpagala <laxman.gdn@gmail.com>
2025-01-05 10:33:49 +0530

    Shell-script-practice

--- 06-variables.sh
+++ 06-variables.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "Please enter your username::"
-read USERNAME
+read -s USERNAME
 
 echo "Please enter your password::"
-read PASSWORD
+read -s PASSWORD
 
 echo "username: $USERNAME"
 echo "password: $PASSWORD"
