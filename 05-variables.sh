029cf28ec604cff77bf11242639c856166700a82
Lakshmanpagala <laxman.gdn@gmail.com>
2025-01-05 10:23:21 +0530

    Shell-script-practice

--- 05-variables.sh
+++ 05-variables.sh
@@ -4,12 +4,13 @@ NUM1=5
 NUM2=6
 SUM=$((NUM1+NUM2))
 DATE=$(date +%Y-%m-%d-%H-%M-%S)
-MOVIES=$(bahubali maghadeera darling)
+MOVIES=$( bahubali maghadeera darling )
 
 echo "script started executing at::$DATE"
+
 echo "Sum of num1 and num2::$SUM"
 
-echo "first movie:: $[0]"
-echo "second movie:: $[1]"
-echo "third movie:: $[2]"
-echo "All movies:: $@"
+echo "first movie:: $MOVIES[0]"
+echo "second movie:: $MOVIES[1]"
+echo "third movie:: $MOVIES[2]"
+echo "All movies:: $MOVIES[@]"
