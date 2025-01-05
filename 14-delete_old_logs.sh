3f50817907b85c89618b300d1b3cd5912bb92d0a
Lakshmanpagala <laxman.gdn@gmail.com>
2025-01-05 13:10:02 +0530

    Shell-script-practice

--- 14-delete_old_logs.sh
+++ 14-delete_old_logs.sh
@@ -11,15 +11,6 @@ LOGS_FOLDER="/var/log/shell_script-old-logs1"
 LOGS_FILE=$(echo $0 | cut -d "." -f1)
 LOGS_FILE_NAME="$LOGS_FOLDER/$LOGS_FILE-$TIMESTAMP.log"
 
-USERID=$(id -u)
-VALIDATE(){
-    if [ $1 -ne 0 ]
-    then
-        echo -e "$2.... $R FAILURE $N"
-    else
-        echo -e "$2.... $G SUCSESS $N"
-    fi
-}
 
 
 sudo mkdir -p /var/log/shell_script-old-logs1
@@ -29,4 +20,13 @@ echo -e "script started executing at:: $TIMESTAMP" &>>$LOGS_FILE_NAME
 
 FILES_TO_DELETE=$(find $SOURSE_DIR -name "*.log" -mtime +14)
 echo "Files to be deleted:: $FILES_TO_DELETE"
- 
+
+while read -r filepath  # here file path is variable name u can give any name
+do
+    echo "Deleting file: $filepath  &>>$LOGS_FILE_NAME
+
+    rm -rf $filepath
+
+    echo "Deleted file:: $filepath"
+
+done <<< $FILES_TO_DELETE  
