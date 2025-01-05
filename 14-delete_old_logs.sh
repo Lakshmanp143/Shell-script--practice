58a4235e5bfdacd83e367b62bec965432b102a4f
Lakshmanpagala <laxman.gdn@gmail.com>
2025-01-05 12:56:00 +0530

    Shell-script-practice

--- 14-delete_old_logs.sh
+++ 14-delete_old_logs.sh
@@ -7,7 +7,7 @@ N="\e[0m"
 
 SOURSE_DIR="/home/ec2-user/app-logs"
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
-LOGS_FOLDER="/var/log/shell_script-old-logs"
+LOGS_FOLDER="/var/log/shell_script-old-logs1"
 LOGS_FILE=$(echo $0 | cut -d "." -f1)
 LOGS_FILE_NAME="$LOGS_FOLDER/$LOGS_FILE-$TIMESTAMP.log"
 
@@ -22,15 +22,8 @@ VALIDATE(){
 }
 
 
-CHECH_ROOT(){
-if [ $USERID -ne 0 ]
-then
-    echo -e " $B You need to be root user to execute tthis script $N"
-    exit 1
-fi
-}
-sudo mkdir -p /var/log/shell_script-old-logs
-sudo chown -R ec2-user:ec2-user shell_script-old-logs
+sudo mkdir -p /var/log/shell_script-old-logs1
+sudo chown -R ec2-user:ec2-user shell_script-old-logs1
 echo -e "script started executing at:: $TIMESTAMP" &>>$LOGS_FILE_NAME
 
 
