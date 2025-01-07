c1297ac371288522983c038d57369e81193b817a
Lakshmanpagala <laxman.gdn@gmail.com>
2025-01-07 21:17:49 +0530

    backup to file

--- disk-usage.sh
+++ disk-usage.sh
@@ -7,4 +7,5 @@ while read r line
 do 
     USAGE=$(echo $line | awk -F " " '{print $6F}')
     PARTITION=$(echo $line | awk -F " " '{print $NF}')
+    echo "partition: $PARTITON, usage: $USAGE"
 done <<< $DISK_USAGE
